2520b219bcf77a135ceb54744c94777160efc256
poshtarenko <catemperor228@gmail.com>
2024-01-16 16:16:23 +0200

    msg

--- src/main/java/com/example/demo/rest/TestController.java
+++ src/main/java/com/example/demo/rest/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-//    very important comment2qfe ........
+//    very important comment2qfe1 ........
 
     @GetMapping("/")
     public ResponseEntity<String> endpoint() {
